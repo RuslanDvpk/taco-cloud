72475722ec756794e372c5b911841783be91619e
RuslanDvpk <ruslan.basharov1994@gmail.com>
2024-03-07 23:27:58 +0500

    Method home added

--- src/main/java/tacos/HomeController.java
+++ src/main/java/tacos/HomeController.java
@@ -1,2 +1,15 @@
-package tacos;public class HomeController {
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.SecureRandom;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
