0b1f192685e07ee81a4e7fd884438d4125ba089a
RuslanDvpk <ruslan.basharov1994@gmail.com>
2024-03-08 21:56:47 +0500

    adding comments

--- src/main/java/tacos/HomeController.java
+++ src/main/java/tacos/HomeController.java
@@ -5,9 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.SecureRandom;
 
+/*
+* Аннотация @Controller используется для определения классов в качестве контроллеров в Spring MVC.
+* Это помогает Spring определить компоненты, которые будут обрабатывать входящие запросы.
+* Её основная цель идентифицировать класс как компонент, доступный механизму сканирования компонентов.
+* В данном случае механизм сканирования автоматически обнаружит его и создаст экземпляр
+* HomeController Как bean-компонент в контесте приложения Spring
+* */
 @Controller
 public class HomeController {
 
+    /*
+    * @GetMapping — Обрабатывает get-запросы
+    *
+    * Аннотация @GetMapping — это просто аннотация которая содержит @RequestMapping(method = RequestMethod.GET).
+    * Она также позволяет более глубоко настроить метод-обработчик.
+    * Ее параметры(они конвертируются в аналогичные параметры @RequestMapping):
+    *
+    * https://habr.com/ru/articles/471140/
+    *
+    * */
     @GetMapping("/")
     public String home(){
         return "home";
--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,9 +3,29 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* Аннотация @SpringBootApplication указывает на то, что это приложение Spring boot
+* Это составная аннотация, объеденяющая три другие аннотации:
+*   @SpringBootConfiguration - Определяет класс как класс конфигурации.
+* Эта аннотация является специализированной формой аннотации @Configuration
+*   @EnableAutoConfiguration - включает автоконфигурацию SpringBoot.
+* Она сообщает Spring Boot о необходимости автоматически настраивать любые компоненты,
+* которые могут понадобиться
+*   @ComponentScan - включает сканирование компонентов.
+* Механизм сканирования позволяет объявлять другие классы с аннотациями, такими как
+* @Component, @Controller, @Service, Чтобы фреймворк Spring автоматически обнаруживал и
+* регистрировал их как компоненты в контексте приложения Spring
+* */
 @SpringBootApplication
 public class TacoCloudApplication {
 
+    /**
+     * Код данного метода шаблонный.
+     * Вызывается метод run() Класса SpringApplication который выполняет фактическую загрузку
+     * метод run() принимает два параметра: класс конфигурации и аргументы коммандной строки
+     * В общем случае необязательно, чтобы это был тот же класс, в котором происходит запуск,
+     * но это самый удобный и распространенный выбор.
+     */
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
     }
