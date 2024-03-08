bd9de88a217fca51536ef6edbfd57eabd02daaf0
RuslanDvpk <ruslan.basharov1994@gmail.com>
2024-03-08 21:57:12 +0500

    adding comments

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -9,12 +9,26 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/*
+* @WebMvcTest специальная тестовая аннотация из Spring Boot
+* которая организует запуск теста в контексте приложения Spring MVC
+* В данном случае она обеспечивает регистрацию класса HomeControler
+* в Spring MVC, чтобы дать возможность отправлять ему запросы.
+*
+* @WebMvcTest также настраивает поддержку тестирования Spring MVC в Spring
+* */
 @WebMvcTest
 public class HomeControllerTest {
 
+    /*
+    * to do: Описать аннотацию @Autowired
+    * */
     @Autowired
     private MockMvc mockMvc;
 
+    /*
+     * to do: Описать аннотацию @Autowired
+     * */
     @Test
     public void testHomePage() throws Exception{
         mockMvc.perform(get("/"))
--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -3,6 +3,11 @@ package tacos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
+* Аннотация @SpringBootTest настраивает JUnit на запуск теста с поддержкой возможностей SpringBoot
+* Это составная аннотация, которая сама снабжена аннотацией @ExtendWith(SpringExtension.class),
+* добавляющей поддержку тестирования Spring в JUnit 5
+* */
 @SpringBootTest
 class TacoCloudApplicationTests {
 
